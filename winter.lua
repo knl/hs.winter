9aaaacb52390efb2cf93099164cf218ad5f12753
Nikola Knezevic <laladelausanne@gmail.com>
2014-12-06 16:38:21 +0100

    Update the in-module documentation

--- winter.lua
+++ winter.lua
@@ -6,24 +6,29 @@
 ---
 --- Usage:
 ---
----     local winter = require "mjolnir.winter"
+---     local wintermod = require "mjolnir.winter"
+---     local winter = wintermod.new()
 ---
 ---     local cmdalt  = {"cmd", "alt"}
 ---     local scmdalt  = {"cmd", "alt", "shift"}
 ---     local ccmdalt = {"ctrl", "cmd", "alt"}
 ---
 ---     -- make the focused window a 200px, full-height window and put it at the left screen edge
----     hotkey.bind(cmdalt, 'h', winter.focused():wide(200):tallest():leftmost():place())
+---     hotkey.bind(cmdalt, 'h', winter:focused():wide(200):tallest():leftmost():place())
 ---
 ---     -- make a full-height window and put it at the right screen edge
----     hotkey.bind(cmdalt, 'j', winter.focused():tallest():rightmost():place())
+---     hotkey.bind(cmdalt, 'j', winter:focused():tallest():rightmost():place())
 ---
 ---     -- full-height window, full-width window, and a combination
----     hotkey.bind(scmdalt, '\\', winter.focused():tallest():resize())
----     hotkey.bind(scmdalt, '-', winter.focused():widest():resize())
----     hotkey.bind(scmdalt, '=', winter.focused():widest():tallest():resize())
+---     hotkey.bind(scmdalt, '\\', winter:focused():tallest():resize())
+---     hotkey.bind(scmdalt, '-', winter:focused():widest():resize())
+---     hotkey.bind(scmdalt, '=', winter:focused():widest():tallest():resize())
 ---
---- *NOTE*: One must start with `winter.focused()` or `winter.window('title')`
+---     -- push to different screen
+---     hotkey.bind(cmdalt, '[', winter:focused():prevscreen():move())
+---     hotkey.bind(cmdalt, ']', winter:focused():nextscreen():move())
+---
+--- *NOTE*: One must start with `winter:focused()` or `winter:window('title')`
 --- and end with a command `move()`, `place()`, `resize()`, or `act()`
 --- (they are all synonyms for the same action). This chain of command
 ---- will return a function that one can pass to hotkey.bind.
@@ -131,7 +136,7 @@ local function init_winter_action(title)
   }
 end
 
---- mjolnir.Winter.focused()
+--- mjolnir.Winter:focused()
 --- Function
 --- Creates a new WinterAction object for the focused window
 function Winter:focused()
